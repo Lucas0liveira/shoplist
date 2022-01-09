70f94f3ab1771a78e01345c6b121deddb1a85403
lucasfreitasbefore <lucas.freitas@before.com.br>
2022-01-09 01:22:55 -0400

    chore: modifica layout da home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,24 +7,57 @@
       </button>
     </div>
 
-    <ul class="list-group" v-if="!loading">
-      <router-link
+    <ul
+      v-if="!loading"
+      class="accordion accordion-flush list-group"
+      id="accordionFlushExample"
+    >
+      <li
+        class="accordion-item list-group-item"
         v-for="list in lists"
         :key="list.id"
-        class="text-decoration-none"
-        :to="`/list/${list.id}`"
       >
-        <li
-          class="list-group-item d-flex justify-content-between align-items-center mb-1"
-        >
-          {{ list.name }}
+        <div class="accordion-header" id="flush-headingOne">
           <span
-            v-if="list.items"
-            class="badge bg-primary rounded-pill text-white"
-            >{{ list.items.length }}</span
+            class="d-flex justify-content-between align-items-center collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseOne"
+            aria-expanded="false"
+            aria-controls="flush-collapseOne"
           >
-        </li>
-      </router-link>
+            {{ list.name }}
+            <span
+              v-if="list.items"
+              class="badge bg-primary rounded-pill text-white"
+              >{{ list.items.length }} item(s)</span
+            >
+          </span>
+        </div>
+        <div
+          id="flush-collapseOne"
+          class="accordion-collapse collapse"
+          aria-labelledby="flush-headingOne"
+          data-bs-parent="#accordionFlushExample"
+        >
+          <div class="accordion-body d-flex justify-content-evenly">
+            <router-link
+              :to="`/list/${list.id}`"
+              tag="button"
+              class="btn btn-warning block mx-1"
+            >
+              <i class="fas fa-pencil"></i>
+            </router-link>
+            <router-link
+              :to="`/shop/${list.id}`"
+              tag="button"
+              class="btn btn-success block mx-1"
+            >
+              <i class="fas fa-shopping-cart"></i>
+            </router-link>
+          </div>
+        </div>
+      </li>
     </ul>
 
     <div v-else class="d-flex justify-content-center">
@@ -46,8 +79,9 @@ export default {
     const loading = ref(true);
     const fullname = ref("");
     const lists = ref([]);
+    const showModal = ref(false);
 
-    async function getProfile() {
+    const getProfile = async () => {
       try {
         loading.value = true;
         store.user = supabase.auth.user();
@@ -68,9 +102,9 @@ export default {
       } finally {
         loading.value = false;
       }
-    }
+    };
 
-    async function getLists() {
+    const getLists = async () => {
       let { data, error, status } = await supabase
         .from("lists")
         .select(`id, items, name, description, user, created_at`)
@@ -84,11 +118,11 @@ export default {
         date: list.created_at,
       }));
       lists.value = cleanLists;
-    }
+    };
 
-    function newList() {
+    const newList = () => {
       router.push("/list");
-    }
+    };
 
     onMounted(() => {
       getProfile();
@@ -100,11 +134,15 @@ export default {
       loading,
       fullname,
       lists,
-
+      showModal,
       newList,
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.list-group-item {
+  cursor: pointer;
+}
+</style>
