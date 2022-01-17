d413d15e74c3e5931d82e608f9f2463dca2157fa
lucasfreitasbefore <lucas.freitas@before.com.br>
2022-01-16 23:58:57 -0400

    feat: componente Return

---
+++ src/components/Return.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header d-flex justify-content-between mb-2">
+    <a @click="$router.go(-1)">
+      <i class="fas fa-arrow-left"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
