bca31a973568c8f82cbf7d36783234ec1dac27f9
lucasfreitasbefore <lucas.freitas@before.com.br>
2022-01-28 23:01:10 -0400

    feat: novos providers de oauth

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="col-6 form-widget">
     <h1 class="header">Bem vindo à Shoppist</h1>
-    <p class="nes-balloon from-right">Sua lista de compras na web!</p>
+    <p class="ballon nes-balloon from-right">Sua lista de compras na web!</p>
     <div class="foot">
+      <div class="wavy">
+        <span style="--i: 1">E</span>
+        <span style="--i: 2">n</span>
+        <span style="--i: 3">t</span>
+        <span style="--i: 4">r</span>
+        <span style="--i: 5">a</span>
+        <span style="--i: 6">r</span>
+        <span style="--i: 7">_</span>
+        <span style="--i: 8">c</span>
+        <span style="--i: 9">o</span>
+        <span style="--i: 10">m</span>
+        <span style="--i: 11">:</span>
+      </div>
       <button
+        v-for="button in buttons"
+        :key="button.name"
         type="submit"
-        class="nes-btn is-primary block flex items-center"
-        @click="signInWithDiscord"
+        class="nes-btn block flex items-center flex-center mb-4"
+        :style="`--shadow: ${button.shadow_color};
+        --bg: ${button.bg_color};
+        --text: ${button.text_color};`"
+        @click="signIn(button.provider)"
         :disabled="loading"
       >
-        Entrar com Discord
-        <i class="nes-icon snes-logo is-small" />
+        {{ button.name }}
       </button>
     </div>
   </div>
@@ -25,12 +42,42 @@ export default {
   setup() {
     const loading = ref(false);
     const router = useRouter();
+    const buttons = ref([
+      {
+        name: "Google",
+        provider: "google",
+        text_color: "#ffffff",
+        bg_color: "#d44538",
+        shadow_color: "#b23121",
+      },
+      {
+        name: "Discord",
+        provider: "discord",
+        text_color: "#f6f6f6",
+        bg_color: "#5865f2",
+        shadow_color: "#1d0bba",
+      },
+      {
+        name: "Twitch",
+        provider: "twitch",
+        text_color: "#ffffff",
+        bg_color: "#9146FF",
+        shadow_color: "#664cda;",
+      },
+      {
+        name: "Twitter",
+        provider: "twitter",
+        text_color: "#E1E8ED",
+        bg_color: "#1DA1F2",
+        shadow_color: "#187cce",
+      },
+    ]);
     const isUserSignedIn = computed(() => Boolean(supabase.auth.user()));
 
-    async function signInWithDiscord() {
+    async function signIn(provider) {
       loading.value = true;
       const { user, session, error } = await supabase.auth.signIn({
-        provider: "discord",
+        provider: provider,
       });
       loading.value = false;
     }
@@ -46,7 +93,8 @@ export default {
     return {
       loading,
       isUserSignedIn,
-      signInWithDiscord,
+      buttons,
+      signIn,
     };
   },
 };
@@ -58,5 +106,52 @@ export default {
   left: 0;
   width: 100%;
   padding: 1rem;
+}
+.wavy {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2rem;
+}
+.wavy span {
+  position: relative;
+  display: inline-block;
+  animation: wave 2s ease-in-out infinite;
+  animation-delay: calc(0.1s * var(--i));
+}
+@keyframes wave {
+  0% {
+    transform: translateY(0px);
+  }
+  20% {
+    transform: translateY(-20px);
+  }
+  40%,
+  100% {
+    transform: translateY(0px);
+    color: black;
+  }
+}
+.ballon {
+  position: relative;
+  animation: bob 3s ease-in-out infinite;
+}
+@keyframes bob {
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(10px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+.nes-btn {
+  color: var(--text);
+  background-color: var(--bg);
+}
+.nes-btn::after {
+  box-shadow: inset -4px -4px var(--shadow) !important;
 }
 </style>
